2f669e872a3591a652f279c5067082e1a26e5151
Dmitrii <fsb2100_1987@mail.ru>
2023-04-02 15:24:46 +0300

    Третье задание

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,10 @@
 function compareArrays(arr1, arr2) {
-  
+    let result = arr1.length === arr2.length && arr1.every((n, i) => n === arr2[i]);
+    return result;
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
-}
+    let result = users.filter( user => user.gender === gender).reduce((accumulator, current, index, array) => accumulator + current.age / array.length, 0);
+    return result;
+       
+  }
